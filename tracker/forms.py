bf7222f2074aefc178899705b818be1e71c2b538
Jozef Hmelar <hmelar.jozef1@gmail.com>
2018-06-05 13:40:26 +0200

    Deleted print, added comment.

--- tracker/forms.py
+++ tracker/forms.py
@@ -4,18 +4,17 @@ from django.forms import widgets
 from tracker.models import Category
 
 class CategoryForm(forms.Form):
-
-    # current_category = forms.ChoiceField(choices=[('adada', 'aaa')])
+    """
+    Form for changing categories on issue_list page.
+    """
 
     def __init__(self, *args, **kwargs):
         """
         Init for this form. Create choices from issue categories.
         """
         super().__init__(*args, **kwargs)
-        # print(dir(self.fields['current_category']))
         choices = [(cat.name, cat.name) for cat in Category.objects.all()]
         choices = [('all', '---')] + choices
-        # choices = (('aa', ''))
         self.fields['current_category'] = forms.ChoiceField(choices=choices, required=False, 
                                                             label="Showing category is:")
 
--- tracker/urls.py
+++ tracker/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'tracker'
 urlpatterns = [
-    path('', views.IssueView.as_view(), name='index'),
+    path('', views.IssueView.as_view()),
     path('<str:category>', views.IssueView.as_view(), name='index'),
     path('change_category/', views.change_category, name='change_cat'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail')
